beb2bfa926fa93c9fd87e4b43f06220e1666c758
unknown <sadmin@LA01251.KWT.PRIV>
2020-08-10 16:01:48 -0700

    Forget Password working

--- src/ch/makery/address/ForgetPasswordController.java
+++ src/ch/makery/address/ForgetPasswordController.java
@@ -26,24 +26,37 @@ public class ForgetPasswordController extends Controller {
 	
 	public void clickSubmitButton()
 	{
-	
-		try
+		
+		if(PassTextField.getText().equals(ConfirmTextfield.getText()))
 		{
-			mainApp.connect("root","");
+		try
+		
+{
+		
+		mainApp.connect("root","");
 		System.out.println("\n\nCreating statement...\n\n");
 		mainApp.stmt = mainApp.conn.createStatement();
 		String sql;
 			
 		
+		
    sql = "UPDATE `newuser` SET password='" + PassTextField.getText() + "' WHERE username = '" + userTextfield.getText() + "' AND userEmail ='" + RemailTextField.getText() + "';";
-   System.out.println("Query:"+sql); 
-//		
+   System.out.println("Query:"+sql); 		
    mainApp.stmt.executeUpdate(sql);
 		clear();
 		
 	} catch (SQLException e) {
 		e.printStackTrace();
 	}
+		}
+		
+		else
+		{
+			ConfirmTextfield.setText("");
+			PassTextField.setText("");
+			
+		}
+	
 	}
 	
 	public void clear()
@@ -52,6 +65,7 @@ public class ForgetPasswordController extends Controller {
         userTextfield.setText("");
         ConfirmTextfield.setText("");
 		PassTextField.setText("");
+		
 	}
 
 }
